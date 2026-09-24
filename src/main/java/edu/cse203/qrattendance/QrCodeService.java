b3051ed64e70675c488e4e6d99c2790981d24172
aragondaanoop2-a11y <aragondaanoop2@gmail.com>
2026-09-24 23:00:00 +0530

    Add src/main/java/edu/cse203/qrattendance/QrCodeService.java

---
+++ src/main/java/edu/cse203/qrattendance/QrCodeService.java
@@ -0,0 +1,26 @@
+package edu.cse203.qrattendance;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
+
+final class QrCodeService {
+    byte[] createPng(String content, int size) throws Exception {
+        Map<EncodeHintType, Object> hints = new EnumMap<>(EncodeHintType.class);
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.M);
+        hints.put(EncodeHintType.MARGIN, 2);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        var matrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, size, size, hints);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(matrix, "PNG", output);
+        return output.toByteArray();
+    }
+}
+
